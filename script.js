dec27a9048f57237063c60433ea9f3b0c00282bc
for-dita <ptexchno@gmail.com>
2026-09-18 23:15:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const beginButton = document.getElementById("beginButton");
 
 beginButton.addEventListener("click", function () {
-    document.getElementById("memories").scrollIntoView({
-        behavior: "smooth"
-    });
+
+
+document.getElementById("memories").scrollIntoView({
+    behavior: "smooth"
 });
+
+
+});
+
