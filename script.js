c7de248ba02fb5f19cc938bbc6430211acb06cf8
for-dita <ptexchno@gmail.com>
2026-09-18 23:35:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ beginButton.addEventListener("click", function () {
         behavior: "smooth"
     });
 });
+
+
+const photoButton = document.getElementById("photoButton");
+const photoOverlay = document.getElementById("photoOverlay");
+const closePhoto = document.querySelector(".close-photo");
+
+
+photoButton.addEventListener("click", function () {
+    photoOverlay.classList.add("active");
+});
+
+
+closePhoto.addEventListener("click", function () {
+    photoOverlay.classList.remove("active");
+});
+
+
+photoOverlay.addEventListener("click", function (event) {
+    if (event.target === photoOverlay) {
+        photoOverlay.classList.remove("active");
+    }
+});
